8997d874783d88af216936f1f89938ff76aca8ae
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2025-01-21 07:51:29 -0500

    Creacionn modelo Pelicula , PeliculasConsultadasPage y BuscarPeliculasPage que representan las diferentes pantallas

---
+++ ExamenProgreso3-ELIZABETHGUERRON/BuscarPeliculasPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ExamenProgreso3_ELIZABETHGUERRON;
+
+public partial class BuscarPeliculasPage : ContentPage
+{
+	public BuscarPeliculasPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ExamenProgreso3-ELIZABETHGUERRON/PeliculasConsultadasPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ExamenProgreso3_ELIZABETHGUERRON;
+
+public partial class PeliculasConsultadasPage : ContentPage
+{
+	public PeliculasConsultadasPage()
+	{
+		InitializeComponent();
+	}
+}
